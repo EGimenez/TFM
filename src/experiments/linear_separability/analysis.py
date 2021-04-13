7289f4b432d2835277f977c9a927caa3040bc502
EGimenez <eduardgimenez@yahoo.com>
2021-04-13 14:34:11 +0200

    We upload code for linear separability analysis

---
+++ src/experiments/linear_separability/analysis.py
@@ -0,0 +1,98 @@
+# usage:
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import platform
+from numpy import dot
+from numpy.linalg import norm
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix
+from utils.FeatureProvider import FeatureProvider
+from pathlib import Path
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--index', required=False, default=Path() / '..' / 'data' / 'celeba' / 'index' / 'list_attr_celeba_clean.txt',  help='path index file')
+if platform.system() == 'Windows':
+    ap.add_argument('-n', '--np_files', required=False, default=Path('E:/NOT_BACKUP/TFM') / 'data' / 'celeba' / 'np', help='path directory containing the np files')
+else:
+    ap.add_argument('-n', '--np_files', required=False, default=Path() / '..' / 'data' / 'celeba' / 'np', help='path directory containing the np files')
+ap.add_argument('-r', '--result', required=False, default=Path() / '..' / 'data' / 'celeba' / 'results' / 'linear_analysis' / 'results.json', help='path where result file will be set')
+ap.add_argument('-s', '--sampling', required=False, type=float, help='If sampling, indicate the percentage')
+args = vars(ap.parse_args())
+
+
+def run_perceptron(x, y):
+    # Perform feature scaling
+    sc = StandardScaler()
+    x = sc.fit_transform(x)
+
+    perceptron = Perceptron(random_state=0, shuffle=True, class_weight='balanced', fit_intercept=False, verbose=0)
+    perceptron.fit(x, y)
+    predicted = perceptron.predict(x)
+    cm = confusion_matrix(y, predicted)
+    return cm
+
+
+def show_matrix(cm, title):
+    plt.clf()
+    plt.imshow(cm, interpolation='nearest')
+    classNames = ['Negative', 'Positive']
+    plt.title(title)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    tick_marks = np.arange(len(classNames))
+    plt.xticks(tick_marks, classNames, rotation=45)
+    plt.yticks(tick_marks, classNames)
+    s = [['TN', 'FP'], ['FN', 'TP']]
+
+    for i in range(2):
+        for j in range(2):
+            plt.text(j, i, str(s[i][j])+" = "+str(cm[i][j]))
+    plt.show()
+
+
+def eval_features(fp: FeatureProvider, result: Path):
+    features = fp.get_features()
+    results = dict()
+
+    for f, i in zip(features, range(len(features))):
+        print(f + ': ' + str(i+1) + '/' + str(len(features)))
+        images_0 = fp.get_np({'Blond_Hair': 0}, sample=0.1, sample_max=20)
+        images_1 = fp.get_np({'Blond_Hair': 1}, sample=0.1, sample_max=20)
+
+        y_0 = np.zeros((len(images_0), 1))
+        y_1 = np.ones((len(images_1), 1))
+
+        x = np.concatenate([[*images_0], [*images_1]])
+        y = np.append(y_0, y_1)
+
+        # Run Perceptron
+        cm = run_perceptron(x, y)
+        # show_matrix(cm, f + ': Confusion Matrix - Entire Data')
+        print(cm)
+
+        # Cosine
+        images_0 = np.average(images_0, axis=0)
+        images_1 = np.average(images_1, axis=0)
+
+        cos_sim = dot(images_0, images_1)/(norm(images_0)*norm(images_1))
+        print(cos_sim)
+
+        results[f] = {'cm': cm.tolist(), 'cos': str(cos_sim)}
+
+    if not result.parent.exists():
+        result.parent.mkdir()
+
+    with open(result, 'w') as o:
+        json.dump(results, o)
+
+
+if __name__ == '__main__':
+    fp = FeatureProvider(index=args['index'], np_path=args['np_files'])
+    eval_features(fp, args['result'])
+    print('hola')
---
+++ src/utils/FeatureProvider.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
+class FeatureProvider(object):
+
+    def __init__(self, index, img_path: Path = None, np_path: Path = None):
+        self.index = pd.read_csv(str(index), sep=' ')
+        self.img_path = img_path
+        self.np_path = np_path
+
+    def filter_index(self, features: dict) -> pd.DataFrame:
+        df = self.index.copy()
+        for key, val in features.items():
+            df = df[df[key] == val]
+        return df
+
+    def get_np(self, features: dict, sample=0.1, sample_max=np.Inf):
+        nps = list()
+        df = self.filter_index(features)
+        max_elements = min(sample * len(df) // 1, sample_max)
+        cur_elements = 0
+        indexes = df.index.values
+        new_indexes = np.random.permutation(indexes)
+
+        for i in new_indexes:
+            file_name = df.loc[i, 'img_id']
+            try:
+                nps.append(np.load(str((self.np_path / (file_name[:-4] + '.npy')))))
+                cur_elements += 1
+                if cur_elements % 500 == 0:
+                    print(cur_elements)
+
+                if cur_elements >= max_elements:
+                    break
+            except:
+                pass
+
+        return np.concatenate(nps)
+
+    def get_features(self) -> list:
+        result = self.index.columns.values.tolist()
+        result.remove('img_id')
+        return result
+
+if __name__ == '__main__':
+    fp = FeatureProvider(index=Path() / '..' / 'data' / 'celeba' / 'index' / 'list_attr_celeba_clean.txt',
+                         np_path=Path('E:/NOT_BACKUP/TFM') / 'data' / 'celeba' / 'np')
+
+    images = fp.get_np({'Blond_Hair': 1})
+
+    print('hola')
