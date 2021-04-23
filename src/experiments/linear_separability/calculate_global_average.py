2747d00e95d1d1476cb03f1dd43caa4c1a41bb9c
EGimenez <eduardgimenez@yahoo.com>
2021-04-23 15:59:31 +0200

    calculating global_average

---
+++ src/experiments/linear_separability/calculate_global_average.py
@@ -0,0 +1,33 @@
+# This script is produced to see if the average of all points in the latent space is zero
+from glob import glob
+import numpy as np
+from tqdm import tqdm
+root_path = f'../data/celeba_wild/'
+root_path = f'../data/celeba/'
+
+np_path = root_path + 'np/'
+re_path = root_path + 'results/linear_analysis/'
+
+file_num = 0
+for file in tqdm(glob(np_path + '*.npy')):
+    an_np = np.load(file)
+    an_np = an_np.copy()
+    file_num += 1
+
+    the_average = None
+
+    try:
+        the_average += an_np
+    except:
+        the_average = an_np
+
+the_average = the_average/file_num
+
+print('The average')
+print(np.linalg.norm(the_average))
+
+# The images
+from utils.glow_api import save_decode
+save_decode(the_average, re_path, 'the_average.jpg')
+
+print('hola')
