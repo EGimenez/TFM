2e04ad90d8a7a14b8628ca7f83214cace64bd536
EGimenez <eduardgimenez@yahoo.com>
2021-04-27 11:49:03 +0200

    .

--- src/config.py
+++ src/config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 if platform.system() == 'Windows':
     data_path = Path('E:/NOT_BACKUP/TFM/data/celeba_wild')
-    models_path = Path('C:/Users/EGimenez/ME/projects/BGSE/TFM/projects/TFM/src/models')
-    results_path = Path('E:/NOT_BACKUP/TFM/results/')
+    models_path = Path('./models')
+    results_path = Path('E:/NOT_BACKUP/TFM/results/celeba_wild')
     index_path = Path('E:/NOT_BACKUP/TFM/data/celeba_wild/index')
 else:
     data_path = Path('/home/egimenez/data/celeba_wild')
-    models_path = Path('C:/Users/EGimenez/ME/projects/BGSE/TFM/projects/TFM/src/models')
-    results_path = Path('/home/egimenez/results')
+    models_path = Path('./models')
+    results_path = Path('/home/egimenez/results/celeba_wild')
     index_path = Path('/home/egimenez/data/celeba_wild/index')
--- src/experiments/linear_separability/the_avg.py
+++ src/experiments/linear_separability/the_avg.py
@@ -14,6 +14,7 @@ from sklearn.metrics import confusion_matrix
 from utils.GlowProvider import GlowProvider
 from pathlib import Path
 from multiprocessing import Pool
+import config
 
 
 # construct the argument parser and parse the arguments
